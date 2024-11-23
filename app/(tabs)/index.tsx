7f93920b7934038a4846f274828807bb2831961f
Gabbygabijo <gabrielfashedemi@gmail.com>
2024-11-23 14:41:24 +0100

    Allow user to stay loged in

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,28 @@
-import { Image, SafeAreaView, ScrollView, StyleSheet, useColorScheme } from 'react-native';
+import { StyleSheet, useColorScheme } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import { darkGray, height, mainStyles } from '@/constants/styles';
-import InputField from '@/components/InputField';
 import { useUser } from '@/context/user-contex';
 import HomeScreen from '../(screens)/HomeScreen';
 import Register from '../(screens)/Register';
+import { useEffect } from 'react';
+import getStoredData from '@/utilities/local-data';
 
 
 export default function TabOneScreen() {
   const colorScheme = useColorScheme()
-  const { user } = useUser()
-  
+  const { user, setUser } = useUser()
+  const handleLastLogin = async () => {
+    const lastLogin = await getStoredData("user")
+    if (lastLogin) {
+      const data = JSON.parse(lastLogin)
+      setUser(data)
+    }
+  }
+  useEffect(() => {
+    if (!user) {
+      handleLastLogin()
+    }
+  })
+
   const styles = StyleSheet.create({
 
   });
