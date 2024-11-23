697b165ad31abd34a9c4aad327ecd3ec66094fcb
Gabbygabijo <gabrielfashedemi@gmail.com>
2024-11-23 14:42:56 +0100

    Created a logout modal

---
+++ components/LogoutPopup.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Modal, StyleSheet } from 'react-native'
+
+export default function LogoutPopup({ children, setVisibility, visibility }: {
+  visibility: boolean
+  setVisibility?: () => void
+  children: React.ReactNode
+}) {
+  const styles = StyleSheet.create({
+    container: {
+      // bac
+    }
+  })
+  return (
+    <Modal visible={visibility} transparent >
+      {children}
+    </Modal>
+  )
+}
